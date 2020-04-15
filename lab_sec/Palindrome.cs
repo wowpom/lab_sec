d00251dd97a0e66d2596702bab4644125430ddd0
Valik0805 <56127421+Valik0805@users.noreply.github.com>
2020-04-15 14:53:36 +0700

    Добавьте файлы проекта.

---
+++ lab_sec/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_sec
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrome(this String number)
+        {
+            char[] digit = number.ToCharArray();
+            switch (digit.Length)
+            {
+                case 1:
+                    return false;
+                case 2:
+                    if (digit[0] == digit[1])
+                        return true;
+                    else
+                        return false;
+            }
+
+            for(int i = 0; i <= digit.Length / 2; i++)
+            {
+                if (digit[i] != digit[digit.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ lab_sec/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab_sec
+{
+    class Program
+    {
+        private static void Menu()
+        {
+            Console.WriteLine("Введите число");
+            if (Palindrome.IsPalindrome(Convert.ToString(Console.ReadLine())) == true) 
+            {
+                Console.WriteLine("Это палиндром");
+            }
+            else
+                Console.WriteLine("Это не палиндром\n");
+
+            Console.WriteLine("Вы хотите ввести ещё одно число \n Введите 1 если да");
+            if (Console.ReadLine() == "1")
+                Menu();
+
+        }
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
